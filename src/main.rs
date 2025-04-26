7c3487c72df3fc5e4e55a3a81813299610c9e9a8
atlasHW <atlassong-k@AtlasSk.GESSHA.venture.com.sg>
2025-04-26 21:05:51 +0800

    modify template for create a empty project

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use {{ STM32Family }} as stm32;
 
 #[entry]
 fn main() -> ! {
